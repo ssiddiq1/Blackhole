1b4b10ce0dd91a98c10c13a4775712310e77a3af
ssiddiq1 <ssiddiq1@oberlin.edu>
2026-04-25 23:44:53 +0000

    Blackhole better visuals

---
+++ build/CMakeFiles/Blackhole.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "Blackhole"
+  "Blackhole.pdb"
+  "CMakeFiles/Blackhole.dir/_deps/imgui-src/imgui.cpp.o"
+  "CMakeFiles/Blackhole.dir/_deps/imgui-src/imgui.cpp.o.d"
+  "CMakeFiles/Blackhole.dir/_deps/imgui-src/imgui_draw.cpp.o"
+  "CMakeFiles/Blackhole.dir/_deps/imgui-src/imgui_draw.cpp.o.d"
+  "CMakeFiles/Blackhole.dir/_deps/imgui-src/imgui_tables.cpp.o"
+  "CMakeFiles/Blackhole.dir/_deps/imgui-src/imgui_tables.cpp.o.d"
+  "CMakeFiles/Blackhole.dir/_deps/imgui-src/imgui_widgets.cpp.o"
+  "CMakeFiles/Blackhole.dir/_deps/imgui-src/imgui_widgets.cpp.o.d"
+  "CMakeFiles/Blackhole.dir/src/GLDebugMessageCallback.cc.o"
+  "CMakeFiles/Blackhole.dir/src/GLDebugMessageCallback.cc.o.d"
+  "CMakeFiles/Blackhole.dir/src/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/Blackhole.dir/src/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/Blackhole.dir/src/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/Blackhole.dir/src/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/Blackhole.dir/src/main.cpp.o"
+  "CMakeFiles/Blackhole.dir/src/main.cpp.o.d"
+  "CMakeFiles/Blackhole.dir/src/render.cpp.o"
+  "CMakeFiles/Blackhole.dir/src/render.cpp.o.d"
+  "CMakeFiles/Blackhole.dir/src/shader.cpp.o"
+  "CMakeFiles/Blackhole.dir/src/shader.cpp.o.d"
+  "CMakeFiles/Blackhole.dir/src/stb_image.cpp.o"
+  "CMakeFiles/Blackhole.dir/src/stb_image.cpp.o.d"
+  "CMakeFiles/Blackhole.dir/src/texture.cpp.o"
+  "CMakeFiles/Blackhole.dir/src/texture.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Blackhole.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ shader/bloom_brightness_pass.frag
@@ -0,0 +1,22 @@
+#version 330 core
+
+uniform sampler2D texture0;
+uniform vec2 resolution; // viewport resolution in pixels
+
+const float brightPassThreshold = 1.0;
+const vec3 luminanceVector = vec3(0.2125, 0.7154, 0.0721);
+
+out vec4 fragColor;
+
+void main() {
+  vec2 texCoord = gl_FragCoord.xy / resolution.xy;
+
+  vec4 c = texture(texture0, texCoord);
+
+  float luminance = dot(luminanceVector, c.xyz);
+  luminance = max(0.0, luminance - brightPassThreshold);
+  c.xyz *= sign(luminance);
+  c.a = 1.0;
+
+  fragColor = c;
+}
